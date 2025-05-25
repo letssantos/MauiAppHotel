0040da4d6122586ab3dbe0ba56ebdf9d524ab1f2
letssantos <lesantos212007@gmail.com>
2025-05-25 20:32:38 -0300

    Modificações app

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -1,7 +1,37 @@
-﻿namespace MauiAppHotel
+﻿using MauiAppHotel.Models;
+
+namespace MauiAppHotel
 {
     public partial class App : Application
     {
+        public List<Quarto>lista_quartos= new List<Quarto>()
+        { 
+            new Quarto()
+            {
+                Descricao = "Suíte Master",
+                ValorDiariaAldulto = 150.0,
+                ValorDiariaCrianca = 70.0
+            },
+             new Quarto()
+            {
+                Descricao = "Suíte Mega",
+                ValorDiariaAldulto = 100.0,
+                ValorDiariaCrianca = 50.0
+            },
+              new Quarto()
+            {
+                Descricao = "Suíte Single",
+                ValorDiariaAldulto = 70.0,
+                ValorDiariaCrianca = 45.0
+            },
+               new Quarto()
+            {
+                Descricao = "Suíte Simplista",
+                ValorDiariaAldulto = 30.0,
+                ValorDiariaCrianca = 20.0
+            }
+        };
+
         public App()
         {
             InitializeComponent();
